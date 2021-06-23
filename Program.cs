8a6415993a672ac7623a99960da1dce69f17b15c
Alexander Jaxgård <38723799+BenBrew@users.noreply.github.com>
2021-06-23 12:44:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pw_rewrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please determine the size of your new password by entering the desired amount of letters/numbers in your password.");
+
+            string password = Console.ReadLine();
+
+            char[] toArray = password.ToCharArray();
+            
+
+            
+        }
+
+        private static string[] ReWriter(string[] password);
+
+
+
+    }
+}
